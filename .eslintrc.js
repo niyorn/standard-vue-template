60cd251f43072f73dac928656fd106f2e5b069e1
niyorn <c.niyornram@hotmail.com>
2019-10-20 16:14:59 +0200

    add: Eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,39 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  extends: ["plugin:vue/essential", "@vue/prettier"],
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
-  },
-  parserOptions: {
-    parser: "babel-eslint"
-  }
+    root: true,
+    env: {
+        node: true
+    },
+    extends: ["plugin:vue/essential", "@vue/prettier"],
+    rules: {
+        "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+        "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+        "indent": [
+            "error",
+            4
+        ],
+        "no-tabs": [
+            "error",
+            {
+                "allowIndentationTabs": true
+            }
+        ],
+        "space-before-function-paren": [
+            "error",
+            {
+                "anonymous": "never",
+                "named": "never",
+                "asyncArrow": "always"
+            }
+        ],
+        "arrow-spacing": [
+            "error",
+            {
+                "before": true,
+                "after": true
+            }
+        ]
+    },
+    parserOptions: {
+        parser: "babel-eslint"
+    }
 };
